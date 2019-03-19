244bab61d1bd322dede0c1d556757a473347dc91
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-19 13:23:07 +0000

    returnt he number itself when it is not multiples of neither 3 or 5

--- ch1/index.js
+++ ch1/index.js
@@ -2,5 +2,5 @@ module.exports = (num) => {
   if (num % 15 == 0) return "FizzBuzz";
   if (num % 3 == 0) return "Fizz";
   if (num % 5 == 0) return "Buzz";
-  return '${num}';
+  return `${num}`;
 };
--- ch1/index.test.js
+++ ch1/index.test.js
@@ -1,6 +1,7 @@
 const fizzBuzz = require('./index');
 describe('fizzBuzz()', () => {
   it('returns "FizzBuzz" for multiples of 3 and 5', () => {
+    expect(fizzBuzz(0)).toBe('FizzBuzz');
     expect(fizzBuzz(15)).toBe('FizzBuzz');
     expect(fizzBuzz(30)).toBe('FizzBuzz');
   });
@@ -13,5 +14,10 @@ describe('fizzBuzz()', () => {
     expect(fizzBuzz(5)).toBe('Buzz');
     expect(fizzBuzz(10)).toBe('Buzz');
     expect(fizzBuzz(20)).toBe('Buzz');
+  });
+  it('returns the number itself when it is not multiples 3 or 5', () => {
+    expect(fizzBuzz(-1)).toBe('-1');
+    expect(fizzBuzz(4)).toBe('4');
+    expect(fizzBuzz(97)).toBe('97');
   });
 });
