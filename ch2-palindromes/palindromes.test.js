9e3ca0157faa6b35c3c6231162dceff9d25a624f
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-19 14:05:13 +0000

    add first test for palindromes

---
+++ ch2-palindromes/palindromes.test.js
@@ -0,0 +1,6 @@
+const palindromes = require('./palindromes')
+describe('palindromes()', () => {
+    it('correctly identifies one-word palindromes', () => {
+        expect(palindromes('madam')).toEqual(['madam']);
+    });
+});
