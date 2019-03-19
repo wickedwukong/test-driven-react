a36b15f1b4bbdc7fd21c40b8707c8f82d026c63e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-19 13:17:57 +0000

    return buzz for multiples of 5

--- ch1/index.test.js
+++ ch1/index.test.js
@@ -7,5 +7,11 @@ describe('fizzBuzz()', () => {
   it('returns "Fizz" for multiples of 3', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
     expect(fizzBuzz(9)).toBe('Fizz');
+    expect(fizzBuzz(18)).toBe('Fizz');
+  });
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+    expect(fizzBuzz(10)).toBe('Buzz');
+    expect(fizzBuzz(20)).toBe('Buzz');
   });
 });
