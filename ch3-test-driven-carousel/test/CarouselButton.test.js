942e44d512931e0ead921b3f921a9e539e2edd4c
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-25 14:16:11 +0000

    :sparkles: Initial implementation of CarouselButton

--- ch3-test-driven-carousel/test/CarouselButton.test.js
+++ ch3-test-driven-carousel/test/CarouselButton.test.js
@@ -19,5 +19,18 @@ describe('CarouselButton', () => {
     it('passes `children` through to the <button>', () => {
         expect(wrapper.prop('children')).toBe(text)
     });
+
+    it('passes other props through to the <button>', () => {
+        const onClick = () => {
+        };
+        const className = 'my-carousel-button';
+
+        const dataAction = 'prev';
+
+        wrapper.setProps({onClick, className, 'data-action': dataAction});
+        expect(wrapper.prop('onClick')).toBe(onClick);
+        expect(wrapper.prop('className')).toBe(className);
+        expect(wrapper.prop('data-action')).toBe(dataAction);
+    });
 });
 
