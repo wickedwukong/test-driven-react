c8abac975ffa424b42fd8317e2f72e4ea61c3624
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-27 13:23:36 +0000

    :wrench: Initial Webpack setup

---
+++ ch3-test-driven-carousel/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: 'development', entry: {
+        carousel: './src/Carousel.js',
+    }, module: {
+        rules: [{
+            test: /\.js$/,
+            loader: require.resolve('babel-loader'),
+        },],
+    },
+};
