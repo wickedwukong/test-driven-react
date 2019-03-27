f700303f0cc5d38a02d6e3b17803fb3f76ce914e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-27 13:47:36 +0000

    :wrench: adding webpack html plugin

--- ch3-test-driven-carousel/webpack.config.js
+++ ch3-test-driven-carousel/webpack.config.js
@@ -3,10 +3,17 @@ module.exports = {
     entry: {
         carousel: './src/Carousel.js',
         example: './example/index.js',
-    }, module: {
+    },
+    module: {
         rules: [{
             test: /\.js$/,
             loader: require.resolve('babel-loader'),
         },],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Carousel Example',
+            chunks: ['example'],
+        }),
+    ],
 };
